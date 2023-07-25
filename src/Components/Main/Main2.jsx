7809d8b5dac61f02e35ba211a18fae334074e58c
Pulkit Sachdeva <sachdeva.pulkit2599@gmail.com>
2023-07-25 14:15:35 +0530

    added list field control

--- src/Components/Main/Main2.jsx
+++ src/Components/Main/Main2.jsx
@@ -6,8 +6,6 @@ import Button from '@mui/material/Button';
 
 function Main2() {
 
-    let sub=true;
-    let parent=true;
 
     const data = [
         {
@@ -36,38 +34,101 @@ function Main2() {
         // console.log(element.target.offsetParent.childNodes[2]);
     }
 
-    const checkUncheck = (element) => {
-        element.target.offsetParent.offsetParent.childNodes[2].childNodes.forEach(elem => {
-            if (elem.control.checked !== element.target.checked)
-                elem.childNodes[0].click();
-            // console.log(elem.childNodes[0]);
-        });
+    const [listHead, setListHead] = useState([]);
+    const [subListHead, setSubListHead] = useState([]);
+
+    useEffect(() => {
+        document.querySelectorAll('.listHead').forEach(element => {
+            listHead.push(true);
+        })
+        document.querySelectorAll('.subList').forEach((element, index) => {
+            subListHead.push([]);
+            element.childNodes.forEach(elem => {
+                subListHead[index].push(true);
+            })
+            // console.log(element);
+        })
+    }, [])
+
+
+    const checkUncheck = (element, deptIndex) => {
+        let newArr = [...listHead];
+        newArr[deptIndex] = !newArr[deptIndex];
+        if(newArr[deptIndex])
+        {
+            setSubListHead(prev => {
+                let temp = [...prev];
+                temp[deptIndex] = [...prev[deptIndex]];
+                temp[deptIndex].forEach((dept,ind)=>{
+                    temp[deptIndex][ind]=true;
+                });
+                console.log(temp);
+                return temp;
+        })}
+        else
+        {
+            setSubListHead(prev => {
+                let temp = [...prev];
+                temp[deptIndex] = [...prev[deptIndex]];
+                temp[deptIndex].forEach((dept,ind)=>{
+                    temp[deptIndex][ind]=false;
+                });
+                console.log(temp);
+                return temp;
+        })}
+        setListHead(newArr);
     }
 
+    const allTrue = (array) => {
+        return array.every(Boolean);
+    }
 
-    const checkAll = (element)=>{
-        element.target.offsetParent.offsetParent.childNodes[2].childNodes.forEach(elem => {
-            if (!elem.control.checked)
-            {
-                allChecked=false;
+    const checkAll = (element, deptIndex, subIndex) => {
+        // console.log(subListHead);
+        setSubListHead(prev => {
+            let newArr = [...prev];
+            newArr[deptIndex] = [...prev[deptIndex]];
+            newArr[deptIndex][subIndex] = !newArr[deptIndex][subIndex];
+            //uncheck dept if subdept is unchecked
+            if (!newArr[deptIndex][subIndex]) {
+                let temp = [...listHead];
+                temp[deptIndex] = false;
+                setListHead(temp);
             }
-                console.log(elem.childNodes[0]);
-                // elem.childNodes[0].click();
-        });
+            else {
+                let temp = true;
+                newArr.forEach(subdept => {
+                    if (!allTrue(subdept)) {
+                        temp = false;
+                    }
+                })
+                if (temp) {
+                    let temp = [...listHead];
+                    temp[deptIndex] = true;
+                    setListHead(temp);
+                }
+                // console.log(temp);
+            }
+            return newArr;
+        })
+    }
+
+    const updateCheck = () => {
+        // console.log(subListHead);
     }
 
     return (
         <div className='main2'>
             <h1>Component 2</h1>
             <div>
-                {data.map(dept => {
+                {data.map((dept, deptIndex) => {
                     return (
                         <FormGroup className='list'>
                             <button onClick={(element) => { showHide(element) }}></button>
-                            <FormControlLabel control={<Checkbox defaultChecked onClick={(element) => { checkUncheck(element) }} />} label={dept.department} className='listHead' style={{ margin: "0px 20px" }} />
+                            <FormControlLabel control={<Checkbox checked={listHead[deptIndex] === undefined ? true : listHead[deptIndex]} onClick={(element) => { checkUncheck(element, deptIndex) }} />} label={dept.department} className='listHead' style={{ margin: "0px 20px" }} />
                             <div className='subList'>
-                                {dept.sub_departments.map(subElement => {
-                                    return (<FormControlLabel control={<Checkbox defaultChecked onClick={(element) => { checkAll(element) }} />} label={subElement} style={{ margin: "0px 40px" }} />)
+                                {dept.sub_departments.map((subElement, subIndex) => {
+                                    return (<FormControlLabel control={<Checkbox checked={subListHead.length === 0 ? true : subListHead[deptIndex][subIndex]} onClick={(element) => { checkAll(element, deptIndex, subIndex) }} onChange={updateCheck} />} label={subElement} style={{ margin: "0px 40px" }} />)
                                 })}</div>
                         </FormGroup>
                     )
